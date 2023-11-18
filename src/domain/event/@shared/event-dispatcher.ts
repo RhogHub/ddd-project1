f28134bfa4514f4bf36c6ae0fb18e5399a770887
RhogHub <rhog.dev@gmail.com>
2023-11-18 17:53:13 -0300

    implementing domain events

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,19 +3,40 @@ import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
-    notify(event: EventInterface): void {
-        
-    }
+
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }   
 
     register(eventName: string, eventHandler: EventHandlerInterface): void {
-        
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+        this.eventHandlers[eventName].push(eventHandler);
     }
 
     unregister(eventName: string, eventHandler: EventHandlerInterface): void {
-        
+        if (this.getEventHandlers[eventName]) {
+            const index = this.eventHandlers[eventName].indexOf(eventHandler);
+            if (index !== -1) {
+                this.eventHandlers[eventName].splice(index, 1);
+            }
+        }
     }
 
     unregisterAll(): void {
-        
+        this.eventHandlers = {};
     }
+
+    notify(event: EventInterface): void {
+        const eventName = event.constructor.name;
+        if (this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName].forEach((eventHander) => {
+                eventHander.handle(event);
+            });
+        }
+    }
+
 }
--- src/domain/event/Product/handler/send-email-when-product-is-created.handler.ts
+++ src/domain/event/Product/handler/send-email-when-product-is-created.handler.ts
@@ -4,6 +4,7 @@ import ProductCreatedEvent from "../product-created.event";
 export default class SendEmailWhenProductIsCreatedHandler implements 
 EventHandlerInterface<ProductCreatedEvent> {
     handle(event: ProductCreatedEvent): void {
-        console.log(`sending email to ${event.eventData.email}`);
+        //console.log(`sending email to ${event.eventData.email}`);
+        console.log(`sending email to .....`); //Enviando para o Rabbitmq
     }
 }
